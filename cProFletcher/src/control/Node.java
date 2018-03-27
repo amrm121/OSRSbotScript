104072f31529e592fb2e0881b5c6a1e5db5d5442
Alexandre Moreno Ribeiro Magalhães <amrm@cin.ufpe.br>
2018-03-27 14:34:31 -0300

    explaining

--- cProFletcher/src/control/Node.java
+++ cProFletcher/src/control/Node.java
@@ -2,15 +2,15 @@ package control;
 
 public abstract class Node {
 	
-	public abstract boolean validate();
+    public abstract boolean validate(); //node is valid to use, like in a FSM I can go to this state by the actions I'm executing in my actual state
 
-    public abstract void execute();
+    public abstract void execute(); //execute the node
 
-    public abstract int priority();
+    public abstract int priority(); //define node priority
 
-    public abstract String getName();
+    public abstract String getName(); //get node name
     
-    public abstract void countM(int x);
+    public abstract void countM(int x); //for nodes which I need an item count 
     
     public abstract int getCount();
     
